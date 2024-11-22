925afecd90844675b11dd1c7d1471de810def91b
MathisPipart <117630102+MathisPipart@users.noreply.github.com>
2024-11-22 22:10:07 +0100

    mail syntax verification

--- src/main/java/org/example/mail_client/controller/LogController.java
+++ src/main/java/org/example/mail_client/controller/LogController.java
@@ -7,20 +7,31 @@ import javafx.stage.Stage;
 import javafx.scene.control.*;
 import org.example.mail_client.MailApplication;
 
+import java.util.regex.Pattern;
+
 public class LogController {
     private Stage logStage;
 
     @FXML
     private TextField mailTextField;
+    @FXML
+    private Label invalidMailLabel;
 
     @FXML
     public void openMailBoxStage() throws Exception {
+        String mail = mailTextField.getText();
+
+        // mail syntax verification
+        if (!isValidEmail(mail)) {
+            invalidMailLabel.setVisible(true);
+            return;
+        }
+
         FXMLLoader fxmlLoader = new FXMLLoader(MailApplication.class.getResource("mailBox-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
 
         MailBoxController mailBoxController = fxmlLoader.getController();
         //Pass the login mail to the mail box
-        String mail = mailTextField.getText();
         mailBoxController.setReceivedMailText(mail);
 
         Stage stage = new Stage();
@@ -35,5 +46,10 @@ public class LogController {
 
     public void setLogStage(Stage stage) {
         this.logStage = stage;
+    }
+
+    private boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        return Pattern.matches(emailRegex, email);
     }
 }
